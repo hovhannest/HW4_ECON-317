15ceebe8b92e94728531e723fb56d24804a777c8
Hovhannes Tsakanyan <hovhannes.ht@gmail.com>
2017-06-26 20:51:48 +0400

    duration formating

--- Utils.py
+++ Utils.py
@@ -6,5 +6,10 @@ def formatViews(views_text):
     return int(float(views_text))
 
 def formatDuration(duration):
-    # TODO: get duration in sec (with regex?)
-    return duration
+    duration = duration.replace("- Duration: ", "")
+    l = duration.split(':')
+    sec = 0
+    c = len(l)
+    for i in range(c):
+        sec += int(float(l[i]))*pow(60, c - i - 1)
+    return sec
